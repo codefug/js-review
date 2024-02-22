167f5c7b5e4416d7e7f9db0c5a8c194a89b71119
codefug <robot9917@naver.com>
2024-02-22 16:04:12 +0900

    complete calculator.js

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// 커피를 만들 때 들어가는 재료들의 칼로리는 다음과 같습니다.
+
+// 에스프레소 : 10 kCal
+// 우유 : 170 kCal
+// 초코시럽 : 50 kCal
+// 휘핑크림 : 60 kCal
+
+// 우리가 판매했던 메뉴들의 칼로리를 한번 계산해 보려고 하는데요, 각 재료의 이름을 변수 이름으로 사용하여, 메뉴들의 칼로리를 저장해 주세요.
+// 변수를 사용하는 테스트 코드는 이미 작성되어 있으니 별도로 편집하지 않으셔도 됩니다. 여러분은 변수를 선언하고 값을 입력해 주세요!
+
+// 변수를 제대로 작성했다면 콘솔에는 아래와 같이 출력되어야 합니다.
+
+// 여기에 코드를 작성하세요
+const espresso= 10;
+const milk=170;
+const chocolateSyrup=50;
+const whippedCream=60;
+// 메뉴별 칼로리 테스트
+console.log(espresso); // 에스프레소 칼로리
+console.log(espresso + milk); // 라떼 칼로리
+console.log(espresso + chocolateSyrup + milk); // 모카 칼로리
+console.log(espresso + chocolateSyrup + milk + whippedCream); // 모카(휘핑 추가) 칼로리
