d1a693a9c1c9c4f1bb7e434721a1eab4996bd8a7
codefug <robot9917@naver.com>
2024-02-26 10:49:12 +0900

    add Array

---
+++ jsAdvanced/Array.js
@@ -0,0 +1,21 @@
+// 실습 설명
+
+// let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+// 여기, dataType 이라는 이름의 6개의 요소를 가진 배열이 있습니다.
+// 이 배열의 요소들을 모두 출력하는 프로그램을 작성해 보세요.
+// 반복문의 개념을 활용하시면 어렵지 않게 해결할 수 있습니다.
+
+let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+
+// 여기에 코드를 작성하세요
+dataType.forEach((value)=>console.log(value));
+// 실습 결과
+// 코드를 잘 작성하셨다면, 다음과 같이 출력되어야 합니다.
+
+
+// number
+// string
+// boolean
+// null
+// undefined
+// object
